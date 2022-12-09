2ce6024ea3d8176e6db13dc3b4c113640199f97b
fengnovo <fengnoku@gmail.com>
2022-12-09 22:05:15 +0800

    feat:  monaco-editor

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import lottie from "lottie-web";
 import a1 from "./public/loading.json";
 import a2 from "./public/data.json";
 import a3 from "./public/404.json";
+import './style.css';
+
+
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import * as monaco from 'monaco-editor'
 
 const idFlow = document.getElementById("id-flow");
 const id404 = document.getElementById("id404");
@@ -27,3 +36,48 @@ lottie.loadAnimation({
     autoplay: true,
     animationData: a3,
 });
+
+
+self.MonacoEnvironment = {
+    getWorker(_, label) {
+        if (label === 'json') {
+            return new jsonWorker()
+        }
+        if (['css', 'scss', 'less'].includes(label)) {
+            return new cssWorker()
+        }
+        if (['html', 'handlebars', 'razor'].includes(label)) {
+            return new htmlWorker()
+        }
+        if (['typescript', 'javascript'].includes(label)) {
+            return new tsWorker()
+        }
+        return new EditorWorker()
+    },
+}
+let editor;
+
+const init = () => {
+    monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+        noSemanticValidation: true,
+        noSyntaxValidation: false,
+    })
+    monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+        target: monaco.languages.typescript.ScriptTarget.ES2020,
+        allowNonTsExtensions: true,
+    })
+
+    editor = monaco.editor.create(document.getElementById('codeEditBox'), {
+        value: '',//编辑器初始显示文字
+        language: 'javascript',//语言支持自行查阅
+        theme: 'vs-dark',//官方自带三种主题vs, hc-black, or vs-dark
+        demoautomaticLayout: true,//自动布局
+    })
+
+    // 监听值的变化
+    editor.onDidChangeModelContent(() => {
+        const value = editor.getValue() //给父组件实时返回最新文本
+        console.log(value);
+    })
+}
+init();
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,32 @@
 import { defineConfig } from 'vite'
+// import { resolve } from 'path'
+// import MonacoEditorNlsPlugin, {
+//     esbuildPluginMonacoEditorNls,
+// } from 'vite-plugin-monaco-editor-nls';
+// import Inspect from 'vite-plugin-inspect';
+
+// const zh_CN = require('vscode-loc.git/i18n/vscode-language-pack-zh-hans/translations/main.i18n.json')
 
 export default defineConfig({
     base: '/lottie-test/',
+    // resolve: {
+    //     alias: {
+    //         '@': resolve('./src'),
+    //     },
+    // },
+    // build: {
+    //     sourcemap: true,
+    // },
+    // optimizeDeps: {
+    //     /** vite 版本需要大于等于2.3.0 */
+    //     esbuildOptions: {
+    //         plugins: [
+    //             esbuildPluginMonacoEditorNls(),
+    //         ],
+    //     },
+    // },
+    // plugins: [
+    //     Inspect(),
+    //     MonacoEditorNlsPlugin(),
+    // ],
 })
