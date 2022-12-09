4f3b5085d05a27ebcfbf50adee325722a3c78f28
fengnovo <fengnoku@gmail.com>
2022-12-09 17:16:19 +0800

    chore: 重新修改结构

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import lottie from "lottie-web";
+import a1 from "./public/loading.json";
+import a2 from "./public/data.json";
+import a3 from "./public/404.json";
 
-createApp(App).mount('#app')
+const idFlow = document.getElementById("id-flow");
+const id404 = document.getElementById("id404");
+const idLoading = document.getElementById("id-loading");
+lottie.loadAnimation({
+    container: idLoading,
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    animationData: a1,
+});
+lottie.loadAnimation({
+    container: idFlow,
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    animationData: a2,
+});
+lottie.loadAnimation({
+    container: id404,
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    animationData: a3,
+});
